4b032e67de9f8b7660e6c7a5e659680e1ad1406e
shauryajain2067 <77152865+shauryajain2067@users.noreply.github.com>
2022-03-01 11:08:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width:350,
+height:300,
+image_format:'png',
+png_quality:90,
+});
+camera=document.getElementById("camera")
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">'
+    })
+}
+
+console.log("ml5",ml5.version);
+
+var classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/djf0L9urs/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('modelLoaded')
+}
